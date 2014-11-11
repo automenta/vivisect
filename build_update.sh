bd05063bcb92c6055fd81825543ffb2b005ac31d
sseehh <1s1e1h1@gmail.com>
2014-11-10 22:44:39 -0500

    build update script for copying built jar to another project

---
+++ build_update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#file helpful for local development until mvn setup is stabilized
+
+while true; do
+
+inotifywait -e close_write target/vivisect-1.0-SNAPSHOT.jar && \
+cp target/vivisect-1.0-SNAPSHOT.jar ../opennars/lib/gui/vivisect.jar
+
+done
